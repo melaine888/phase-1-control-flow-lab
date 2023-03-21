cd5d391c0909c3b79f94acb9dba54675a12901e7
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-03-21 13:15:54 +0300

    DONE WITH ASSIGNMENT

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
   // Write your code here!
+
+  if (ride <= 400) {
+    return scuberGreetingForFeet = "This one is on me!";
+  } else if (ride > 2500) {
+    return scuberGreetingForFeet = "No can do.";
+  } else if (2000 < ride) {
+    return scuberGreetingForFeet = "I will gladly take your thirty bucks.";
+  } 
+
+
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let result;
+  result = (city=='NYC') ? ("Ok, sounds good.") : ("No go.") ;
+ return result;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  var charm;
+
+  
+  switch(tip) {
+    case "generous":
+      charm = "Thank you so much.";
+      break;
+    case "not as generous":
+      charm = "Thank you.";
+      break;
+    case "thanks for everything":
+      charm = "Bye.";
+      break;
+      
+    default:
+      charm = "I have never heard of that fruit...";
+  }
+return charm;
 }
+
